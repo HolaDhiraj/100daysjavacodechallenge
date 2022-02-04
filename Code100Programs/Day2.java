bc8cd9c6a79b8bd906f25579bc985ca26cdf8407
HolaDhiraj <dheerazbhattarai@gmail.com>
2022-02-04 23:57:45 +0545

    2

---
+++ Code100Programs/Day2.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Code100Programs;
+
+/**
+ *
+ * @author Dhiraj Bhattarai
+ */
+ import java.io.PrintWriter;
+public class Day2 {
+    public static void main(String [] args){
+        
+        //system.out.println("hello");
+        
+        //PrintWriter pt = new PrintWriter(System.out, true);
+        //String stmt = "codeHUb_DheRU" ;
+        //pt.println("Welcome to my channel " + stmt);
+        
+        //Arthametic opertaion 
+       
+        float x = 10;
+        float y = 6;
+        
+        float r1, r2, r3;
+        
+        r1 = x+y;
+        r2 = x-y;
+        r3 = x*y;
+        float r4 = x/y;
+        
+        System.out.println("Addition: " + r1);
+        System.out.println("Subtraction: " + r2);
+        System.out.println("Multiplication: " + r3);
+        System.out.println("Division : " + r4);
+        
+        
+        
+        
+    }
+    
+}
+    
