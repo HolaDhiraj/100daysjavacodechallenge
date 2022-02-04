7ebb49f8105c09fcaa921e333c1978420207fa93
HolaDhiraj <dheerazbhattarai@gmail.com>
2022-02-04 18:13:19 +0545

    first

---
+++ Code100Programs/Day1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Code100Programs;
+
+/**
+ *
+ * @author Dhiraj Bhattarai
+ */
+public class Day1 {
+    public static void main(String[] args){
+        
+        // this is the comment
+        // sout+tab helps in typing system.out.println wghich helps to print the line
+        
+        System.out.println("Hello World");// println means print in next line
+        System.out.println("Welcome to my Channel CodeHub_DheRu");
+        //System.out.print("hello");// only print means print in same line
+        //System.out.print("hello");
+        
+    }
+}
